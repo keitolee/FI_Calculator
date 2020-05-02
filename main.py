73868eb002cfa00e93070d4c576bf07ac1bd4909
keitolee <keitolee96@gmail.com>
2020-05-02 00:17:23 -0700

    compound interest function

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ def add_income( salary, eq_int, cash_int, other ):
 #calculates MONTHLY expenses
 
 def add_expenses( rent, food, utilities, car, other ):
-     return rent + food + utilities + car + other
+    return rent + food + utilities + car + other
+
+def compound_interest( principal, rate, periods):
+    total = principal * ( pow( ( 1 + rate ), periods ) - 1 )
+    return "{:.2f}".format(total)
 
 cash = 15000
 equity = 90000
@@ -29,8 +33,8 @@ per_loans = 0
 otherL = 0
 
 salary = 100000
-eq_int = (equity * 0.07)/12 #assuming 7% return on equities calculated yearly
-cash_int = (cash * 0.02)/12 #assuming 2% return on cash calculated yearly
+eq_int = (equity * 0.07)/12 
+cash_int = (cash * 0.02)/12 
 otherI = 200
 
 rent = 700
@@ -60,3 +64,5 @@ if monthly_income <= monthly_expenses:
     print("Income is less than expenses therefore, unable to build wealth. \n Start by increasing income and/or lowering expenses" )
 else:
     print( "Total amout required to save for ", desired_retirement_length, " years at ", desired_retirement_expenses, " a month: $", retirement_total )
+
+ 
