b5dec8c843dcc75ab85f14a6257a87815cd7b0ca
keitolee <keitolee96@gmail.com>
2020-05-01 22:53:09 -0700

    fixed total assets/liabilities

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 
 def add_assets( cash, equity, bonds, RE, otherA ):
-    return cash + equity + bonds + RE + other
+    return cash + equity + bonds + RE + otherA
 
 def add_liabilities( stu_loans, mort, credit, per_loans, otherL ):
-    return stu_loans + mort + credit + per_loans + other
+    return stu_loans + mort + credit + per_loans + otherL
 
-def add_expenses():
-
-
-def add_income():
 
 cash = 10000
 equity = 30000
@@ -20,10 +16,12 @@ stu_loans = 30000
 mort = 0
 credit = 2000
 per_loans = 0
-otherLL = 0
+otherL = 0
 
 
 curr_assets = add_assets ( cash, equity, bonds, RE, otherA )
 curr_liabilities = add_liabilities( stu_loans, mort, credit, per_loans, otherL )
 
 
+print("Total Current Assets: ", curr_assets)
+print("Total Current Liabilities: ", curr_liabilities)
