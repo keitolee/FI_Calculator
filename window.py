3ab7620bdd444d27ca79c0c518b8b0592fa37d9a
keitolee <keitolee96@gmail.com>
2020-05-03 22:43:06 -0700

    added 2 buttons for each option

--- window.py
+++ window.py
@@ -1,10 +1,24 @@
 from tkinter import *
 
+def clicked1():
+    lbl.configure(text="Pressed Retire Now")
+
+def clicked2():
+    lbl.configure(text="Pressed Retire at a specific age")
+
 window = Tk()
 
 window.title("Financial Independence Calculator")
 
-lbl = Label(window, text="Welcome to The Financial Independence Calculator", font=("Calibri", 30))
+window.geometry('600x500')
+
+lbl = Label(window, text="Welcome" )
 lbl.grid(column=0, row=0)
+
+btn1 = Button( window, text="Retire NOW", command=clicked1 )
+btn1.grid(column=0, row=1)
+
+btn1 = Button( window, text="Retire at a specific age", command=clicked2 )
+btn1.grid(column=1, row=1)
 
 window.mainloop()
