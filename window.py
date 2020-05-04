a97ad543de8cf6da194cf2bfa6cb5bb9dbcf988e
keitolee <keitolee96@gmail.com>
2020-05-03 23:20:24 -0700

    buttons dissappear after press

--- window.py
+++ window.py
@@ -1,10 +1,15 @@
 from tkinter import *
 
-def clicked1():
+def clicked1(btn1, btn2):
     lbl.configure(text="Pressed Retire Now")
+    btn1.grid_forget()
+    btn2.grid_forget()
 
-def clicked2():
+
+def clicked2(btn1, btn2):
     lbl.configure(text="Pressed Retire at a specific age")
+    btn2.grid_forget()
+    btn1.grid_forget()
 
 window = Tk()
 
@@ -15,10 +20,10 @@ window.geometry('600x500')
 lbl = Label(window, text="Welcome" )
 lbl.grid(column=0, row=0)
 
-btn1 = Button( window, text="Retire NOW", command=clicked1 )
+btn1 = Button( window, text="Retire NOW", command=lambda: clicked1(btn1, btn2) )
 btn1.grid(column=0, row=1)
 
-btn1 = Button( window, text="Retire at a specific age", command=clicked2 )
-btn1.grid(column=1, row=1)
+btn2 = Button( window, text="Retire at a specific age", command=lambda: clicked2(btn2, btn1) )
+btn2.grid(column=1, row=1)
 
 window.mainloop()
